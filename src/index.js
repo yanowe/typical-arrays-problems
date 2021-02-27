d8c5d18156d82f597dc2dd6c0ce8bfab3d3bee1d
yanowe <yanowe@mail.ru>
2021-02-27 20:38:53 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0)) return 0;
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  return array.sort(compareNumeric)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0)) return 0;
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  return array.sort(compareNumeric)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0)) return 0;
+  let length = array.length;
+  console.log(`length = ${length}`);
+  let reduced = array.reduce(function(a,b){
+    return a + b;
+    });
+  return (reduced / length);
 }
